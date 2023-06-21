64415aeb60100faec286a0c151f898e30a153e3b
lun2ee <128441642+lun2ee@users.noreply.github.com>
2023-06-21 15:56:51 +0900

    기ㄹ과제 ㅜㅜ

---
+++ w14_final/stp 복사본/script/intersectionObserver.js
@@ -0,0 +1,32 @@
+let observerOption = {
+  root: null,
+  rootMargin: "100px",
+  threshold: 0.75,
+};
+let observerCallback = (observingTargets) => {
+  observingTargets.forEach(
+    (eachObservingTarget) => {
+      if (eachObservingTarget.isIntersecting) {
+        eachObservingTarget.target.classList.add(
+          "triggered"
+        );
+      } else {
+        eachObservingTarget.target.classList.remove(
+          "triggered"
+        );
+      }
+    }
+  );
+};
+
+let observer = new IntersectionObserver(
+  observerCallback,
+  observerOption
+);
+let elems = document.querySelectorAll(
+  ".animTrigger"
+);
+console.log(elems);
+elems.forEach((eachElem) => {
+  observer.observe(eachElem);
+});
